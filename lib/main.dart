b2920e6956e58bc10774997d97216e1f2bdeddb0
Nafis71 <nhtonmoy2@gmail.com>
2024-08-08 19:26:22 +0600

    - status bar color change

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:crafty_bay/app/app.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(DevicePreview(builder: (_)=> const CraftyBay()));
 }
--- lib/themes/appbar_style.dart
+++ lib/themes/appbar_style.dart
@@ -1,8 +1,13 @@
+import 'package:crafty_bay/themes/app_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AppbarStyle{
-  static AppBarTheme getLightAppBarTheme() => const AppBarTheme(
+  static AppBarTheme getLightAppBarTheme() =>   AppBarTheme(
     backgroundColor: Colors.white,
     surfaceTintColor: Colors.white,
+    systemOverlayStyle: SystemUiOverlayStyle(
+      statusBarColor: AppColor.appPrimaryColor,
+    )
   );
 }
