655f423018cd4314f904fab3f5b6722d1209a4f2
Nafis71 <nhtonmoy2@gmail.com>
2024-08-11 04:43:34 +0600

    bottom navigation added

--- lib/features/base_navigation/views/base_navigation_view.dart
+++ lib/features/base_navigation/views/base_navigation_view.dart
@@ -1,6 +1,6 @@
-import 'package:crafty_bay/view_models/profile_view_model.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:gap/gap.dart';
 
 class BaseNavigationView extends StatefulWidget {
   const BaseNavigationView({super.key});
@@ -14,8 +14,61 @@ class _BaseNavigationViewState extends State<BaseNavigationView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body:  Center(
-        child: Text(Get.find<ProfileViewModel>().userModel.firstName.toString()),
+      body: const Center(
+        child: Text("data"),
+      ),
+      bottomNavigationBar: Theme(
+        data: ThemeData(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+        ),
+        child: BottomNavigationBar(
+          iconSize: 36,
+          useLegacyColorScheme: false,
+          backgroundColor: Colors.white,
+          elevation: 10,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Theme.of(context).primaryColor,
+          selectedLabelStyle:
+              const TextStyle(fontSize: 11, fontFamily: "Poppins"),
+          onTap: (index) {
+            if (index == 0) {}
+          },
+          items: const [
+            BottomNavigationBarItem(
+                icon: Column(
+                  children: [
+                    Icon(EvaIcons.homeOutline),
+                    Gap(3),
+                  ],
+                ),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Column(
+                  children: [
+                    Icon(EvaIcons.gridOutline),
+                    Gap(3),
+                  ],
+                ),
+                label: "Category"),
+            BottomNavigationBarItem(
+                icon: Column(
+                  children: [
+                    Icon(Icons.shopping_basket_outlined),
+                    Gap(3),
+                  ],
+                ),
+                label: "Cart"),
+            BottomNavigationBarItem(
+                icon: Column(
+                  children: [
+                    Icon(EvaIcons.giftOutline),
+                    Gap(3),
+                  ],
+                ),
+                label: "Wish"),
+          ],
+        ),
       ),
     );
   }
--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -3,16 +3,17 @@ import 'package:crafty_bay/features/authentication/views/otp_verification_view/o
 import 'package:crafty_bay/features/authentication/views/profile_detail_view/profile_detail_screen.dart';
 import 'package:crafty_bay/features/base_navigation/views/base_navigation_view.dart';
 import 'package:flutter/material.dart';
+
 import '../features/onboarding/views/splash_view/splash_view.dart';
 
 class AppRoutes {
-  static const String splashView = "splashScreen";
-  static const String loginView = "loginScreen";
-  static const String otpVerificationView = "otpVerificationScreen";
-  static const String profileDetailView = "profileDetailScreen";
-  static const String baseNavigationView = "mainNavigationView";
+  static const String splashView = "/";
+  static const String loginView = "/loginScreen";
+  static const String otpVerificationView = "/otpVerificationScreen";
+  static const String profileDetailView = "/profileDetailScreen";
+  static const String baseNavigationView = "/mainNavigationView";
 
-  static MaterialPageRoute? generateRoutes(RouteSettings routeSettings) {
+  static PageRoute? generateRoutes(RouteSettings routeSettings) {
     Map<String, WidgetBuilder> routes = {
       AppRoutes.splashView: (context) => const SplashView(),
       AppRoutes.loginView: (context) => const LoginView(),
