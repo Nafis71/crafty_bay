126da92f220c9c4f54976552ee09de634edc1285
Nafis71 <nhtonmoy2@gmail.com>
2024-08-11 17:54:46 +0600

    code refactored

--- lib/features/base_navigation/views/base_navigation_view.dart
+++ lib/features/base_navigation/views/base_navigation_view.dart
@@ -1,13 +1,7 @@
 import 'package:crafty_bay/features/base_navigation/view_model/base_navigation_view_model.dart';
 import 'package:crafty_bay/features/base_navigation/widgets/bottom_navigation.dart';
-import 'package:crafty_bay/utils/app_assets.dart';
-import 'package:crafty_bay/wrappers/svg_image_loader.dart';
-import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 import 'package:get/get.dart';
-
-import '../widgets/bottom_navigation_bar_item.dart';
 
 class BaseNavigationView extends StatefulWidget {
   const BaseNavigationView({super.key});
