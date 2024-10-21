cf871926f28ef3e690f3a1af59e8a7f3d1b61dd5
Nafis71 <nhtonmoy2@gmail.com>
2024-10-21 16:07:30 +0600

    theme bug fix

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,13 +3,13 @@ import 'package:crafty_bay/core/utils/app_routes.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../core/themes/app_theme.dart';
 import '../core/themes/theme_switcher.dart';
 
 class CraftyBay extends StatefulWidget {
-  const CraftyBay({super.key});
+  final bool? isDarkModeEnabled;
+  const CraftyBay({super.key, this.isDarkModeEnabled});
 
   @override
   State<CraftyBay> createState() => _CraftyBayState();
@@ -21,7 +21,7 @@ class _CraftyBayState extends State<CraftyBay> {
   @override
   void initState() {
     navigatorState = GlobalKey<NavigatorState>();
-    loadTheme();
+    loadTheme(widget.isDarkModeEnabled);
     super.initState();
   }
 
@@ -45,9 +45,7 @@ class _CraftyBayState extends State<CraftyBay> {
     });
   }
 
-  Future<void> loadTheme() async {
-    SharedPreferences localStorage = await SharedPreferences.getInstance();
-    bool? isDarkModeEnabled = localStorage.getBool("isDarkModeEnabled");
+  Future<void> loadTheme(bool? isDarkModeEnabled) async {
     if (isDarkModeEnabled != null && isDarkModeEnabled) {
       Get.find<ThemeSwitcher>().setThemeModeSilently = ThemeMode.dark;
     }
--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,31 @@ import 'package:crafty_bay/app/app.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'core/state_holders/connection_state.dart';
 import 'core/themes/theme_switcher.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  initializePrerequisite();
+  bool? isDarkModeEnabled = await loadTheme();
+  runApp(
+    DevicePreview(
+      enabled: false,
+      builder: (_) => CraftyBay(
+        isDarkModeEnabled: isDarkModeEnabled,
+      ),
+    ),
+  );
+}
+
+void initializePrerequisite() {
   Get.put(NetworkState());
   Get.find<NetworkState>().initialize();
   Get.put(ThemeSwitcher());
-  runApp(DevicePreview(enabled: true, builder: (_) => const CraftyBay()));
+}
+
+Future<bool?> loadTheme() async {
+  SharedPreferences localStorage = await SharedPreferences.getInstance();
+  return localStorage.getBool("isDarkModeEnabled");
 }
